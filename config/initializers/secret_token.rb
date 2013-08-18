b4cd4c230e8e4b2ef7073c57963cb2de88c13c22
spencer handley <spencerhandley@gmail.com>
2013-08-18 08:56:31 -0600

    adding everything to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gosourca::Application.config.secret_key_base = '4bacc03ae9c61b4d2b4de126034125a47d50dd8a9620afe3642e31f13b3ad29da1e053194198c943611c8f59c3a913bccf76aa35b0da1de586176ac3d7ecbdf0'
